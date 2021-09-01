27e09d4a040d45b7d1bbd8565d0028cdec9ac180
Gary Rapoza <68869594+abnvet20202@users.noreply.github.com>
2021-09-01 16:07:26 -0700

    Updated Session 1 Project
    Updated Session 1 Project

---
+++ FSDI 103/session 1/script.js
@@ -0,0 +1,42 @@
+/* console.log ("hello world!");
+console.warn ("Warning.......");
+console.error("Fatal Error!!!");
+console.log ("eat a piece of pizza")
+document.write ("Hello Javascript")
+alert ("Hello world 123456789") */
+
+console.log("Script with 20 variables that prints in HTML");
+
+var husbandName="Gary";
+var wifesName= "Stephanie";
+var lastName= "Rapoza";
+var City= "Henderson";
+var state= "Nevada";
+var country= "United States of America.";
+var kids= "1";
+var kidsSex= "boy";
+var hisStatus= "Retired";
+var herStatus= "Attorney"
+
+var husbandName2="Bruce";
+var wifesName2= "Julie";
+var lastName2= "Wayne";
+var City2= "Gothim";
+var state2= "New Jersey";
+var country2= "United States of America.";
+var kids2= "1";
+var kidsSex2= "girl";
+var hisStatus2= "Super Hero";
+var herStatus2= "Reporter"
+
+
+
+
+document.write( husbandName + " & " + wifesName + " " + lastName + ", " + "you live in " + City + " " + state + " which resides in the " + country + " You have " + kids + " little" + kidsSex + ", " + husbandName + " is " + hisStatus + " and " + wifesName + " works as an " + herStatus + ". " + "Their friends, " + husbandName2 + " & " + wifesName2 + " " + lastName2 + ", " + "live in " + City2 + " " + state2 + " which resides in the " + country2 + " they have " + kids2 + " little " + kidsSex2 + ", " + husbandName2 + " is " + hisStatus2 + " and " + wifesName2 + " works as an " + herStatus2 + ". "  )
+
+
+
+
+
+
+

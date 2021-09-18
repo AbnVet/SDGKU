51b0069e4bce948979fb4000aa2ec09c2f7215cc
Gary Rapoza <68869594+abnvet20202@users.noreply.github.com>
2021-09-18 10:18:02 -0700

    Completed Competency Report

--- FSDI 104/Class 4/Competency Report/scripts/app.js
+++ FSDI 104/Class 4/Competency Report/scripts/app.js
@@ -20,9 +20,10 @@ var counter=0;
 
 class Pet{
 
-    constructor(name,age,gender,breed,service,owner,phone){
+    constructor(name,age,color,gender,breed,service,owner,phone){
         this.name=name;
         this.age=age;
+        this.color=color;
         this.gender=gender;
         this.breed=breed;
         this.service=service;
@@ -32,40 +33,59 @@ class Pet{
     }
 }
 
-//create pets
-var scooby=new Pet("Scooby",60,"Male","Dane","Grooming","Shaggy","555-555-555");
+/////////////////////////PET VARIABLES\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+var scooby=new Pet("Scooby",60,"White","Male","Dane","Grooming","Shaggy","555-555-555");
 saloon.pets.push(scooby);
-var scrappy=new Pet("Scrappy",50,"Male","Mixed","Nails cut","Shaggy","555-555-555");
+var scrappy=new Pet("Scrappy",50,"Black","Male","Mixed","Nails cut","Shaggy","555-555-555");
 saloon.pets.push(scrappy);
-var tweety=new Pet("Tweety Bird",100,"Male","Mixed","Nails cut","bugs bunny","999-555-555");
+var tweety=new Pet("Tweety Bird",100,"yellow","Male","Mixed","Nails cut","bugs bunny","999-555-555");
 saloon.pets.push(tweety)
 
 // getting tha values from the inputs
 var txtName=document.getElementById('petName');
 var txtAge=document.getElementById('petAge');
+var txtColor=document.getElementById('petcolor')
 var txtGender=document.getElementById('petGender');
 var txtBreed=document.getElementById('petBreed');
 var txtService=document.getElementById('petService');
 var txtOwner=document.getElementById('ownerName');
 var txtPhone=document.getElementById('ownerPhone');
 
+/////////////////////////REGISTER FUNCTION\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
 function register(){
-    var thePet = new Pet(txtName.value,txtAge.value,txtGender.value,txtBreed.value,txtService.value,txtOwner.value,txtPhone.value);
-    console.log(thePet);
-    saloon.pets.push(thePet);
-    clear();
-    displayTable();
+
+    if(txtName.value!="" && txtPhone.value!=""){
+        var thePet = new Pet(txtName.value,txtAge.value,txtGender.value,txtBreed.value,txtService.value,txtOwner.value,txtPhone.value);
+        console.log(thePet);
+        saloon.pets.push(thePet);
+        clear();
+        displayTable();
+        // alert("A pet was registered!");
+        var alertElement=document.getElementById("alert");
+        alertElement.classList.remove("hide");
+        setTimeout(function(){
+            alertElement.classList.add("hide");
+        }, 3000);
+        
+
+    }else{
+
+        alert("Please enter the required fields")
+    }
 }
 
+/////////////////////////CLEAR FORM\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
 function clear(){
     txtName.value="";// clearing the input
     txtAge.value="";
+    txtColor.value="";
     txtGender.value="";
     txtBreed.value="";
     txtOwner.value="";
     txtPhone.value="";
 }
 
+/////////////////////////FUNCTION DISPLAY\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
 function display (){
     const petSection=document.getElementById('pets');
     var tmp ="";
@@ -74,6 +94,7 @@ function display (){
                     <h3>${saloon.pets[i].name} </h3>
                     <p>Age: ${saloon.pets[i].age} </p>
                     <p>Gender: ${saloon.pets[i].gender} </p>
+                    <p>Color: ${saloon.pets[i].color} </p>
                     <p>Breed: ${saloon.pets[i].breed} </p>
                     <p>Service: ${saloon.pets[i].service} </p>
                     <p>Owner Name: ${saloon.pets[i].owner} </p>
@@ -87,6 +108,7 @@ function display (){
     petSection.innerHTML=tmp;
 }
 
+/////////////////////////DISPLAY TABLE\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
 function displayTable(){
 //select the tbody section
 
@@ -101,6 +123,7 @@ function displayTable(){
             <tr id=${saloon.pets[i].id}>
                 <td>${saloon.pets[i].name} </td>
                 <td>${saloon.pets[i].age} </td>
+                <td>${saloon.pets[i].color} </td>
                 <td>${saloon.pets[i].gender} </td>
                 <td>${saloon.pets[i].breed} </td>
                 <td>${saloon.pets[i].service} </td>
@@ -117,6 +140,7 @@ function displayTable(){
     table.innerHTML=tr;
 }
 
+///////////////////////// DELETE PET FUNCTION \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
 function deletePet(id){
     var row=document.getElementById(id);
     row.remove(); //will remove element from html
@@ -129,11 +153,38 @@ function deletePet(id){
     saloon.pets.splice(indexDelete,1) //will remove from array
 }
 
+/////////////////////////SEARCH FUNCTION\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+function searchPet(){ //by name
 
+    //create a var, get the element, use .value
+
+    var txtSearch = document.getElementById("searchInput").value;
+    var searchString=txtSearch.toLowerCase();
+   
+    //travel the array to search the string
+    saloon.pets.forEach(pet=>{
+        //compare the search txt string with all the pet names
+        console.log(pet.name);
+        if(pet.name.toLowerCase()===searchString){
+
+                //highlight the result
+            document.getElementById(pet.id).classList.add('highlight');
+        }else{
+            document.getElementById(pet.id).classList.remove('highlight');
+        }
+
+    });
+
+ 
+}
+
+////////////////////////////// Init \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
 function init(){
     console.log("app.js");
     displayTable();
 // Hook Events - trigger functions here
+    document.querySelector(".btn-register").addEventListener("click", register);
+    document.querySelector(".btn-search").addEventListener("click",searchPet);
 
 
 }
